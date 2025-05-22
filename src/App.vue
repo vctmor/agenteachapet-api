6badd758fa9533710eb175b3d498ef394c3a15f6
Victor Moreira <you@example.com>
2025-05-22 11:06:28 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>BuscaPet</h1>
+    <FormPet />
+    <hr />
+    <PetList />
+  </div>
+</template>
+
+<script setup>
+import FormPet from './components/FormPet.vue'
+import PetList from './components/PetList.vue'
+</script>
+
+<style>
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
