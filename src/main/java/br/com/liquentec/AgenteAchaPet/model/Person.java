d67ca3e2c37a648c29242088de5f83481b610256
Victor Moreira <you@example.com>
2025-05-25 10:33:34 -0300

    teste testCreatAndReadAttributes e classe Person ok

--- src/main/java/br/com/liquentec/AgenteAchaPet/model/Person.java
+++ src/main/java/br/com/liquentec/AgenteAchaPet/model/Person.java
@@ -1,5 +1,30 @@
 package br.com.liquentec.AgenteAchaPet.model;
 
+
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name="person")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String email;
+    private String Phone;
+    
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "person")
+    private List<Pet> pets;
     
 }
--- src/test/java/br/com/liquentec/AgenteAchaPet/model/PersonTest.java
+++ src/test/java/br/com/liquentec/AgenteAchaPet/model/PersonTest.java
@@ -1,5 +1,7 @@
 package br.com.liquentec.AgenteAchaPet.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PersonTest {
@@ -7,6 +9,19 @@ public class PersonTest {
     @Test
     void testCreatAndReadAttributes(){
 
-        
+        Person person = new Person();
+
+        person.setId(1L);
+        person.setName("Maria");
+        person.setEmail("maria@email.com");
+        person.setPhone("11965498733");
+        person.setRole(Role.ADVISER);
+
+        assertEquals(1L, person.getId());
+        assertEquals("Maria", person.getName());
+        assertEquals("maria@email.com", person.getEmail());
+        assertEquals("11965498733", person.getPhone());
+        assertEquals(Role.ADVISER, person.getRole());
+
     }
 }
