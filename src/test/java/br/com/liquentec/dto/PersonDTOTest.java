a1d88ccfbd4d86acc62f44134f38f0192333e205
Victor Moreira <you@example.com>
2025-05-25 11:10:11 -0300

    testSetters e testNoArgsConstructor, ok

--- src/test/java/br/com/liquentec/dto/PersonDTOTest.java
+++ src/test/java/br/com/liquentec/dto/PersonDTOTest.java
@@ -1,6 +1,7 @@
 package br.com.liquentec.dto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,7 +20,33 @@ public class PersonDTOTest {
         assertEquals( "maria@email.com",dto.getEmail());
         assertEquals("11999999999", dto.getPhone());
         assertEquals(Role.ADVISER, dto.getRole());
+    }
 
+    @Test
+    void testSetters(){
 
+        PersonDTO dto = new PersonDTO();
+
+        dto.setId(2L);
+        dto.setName("J");
+        dto.setEmail("a@b");
+        dto.setPhone("33");
+        dto.setRole(Role.KEEPER);
+        
+        assertEquals(2L, dto.getId());
+        assertEquals("J", dto.getName());
+        assertEquals("a@b", dto.getEmail());
+        assertEquals("33", dto.getPhone());
+        assertEquals(Role.KEEPER, dto.getRole());
+    }
+
+    @Test
+    void testNoArgsConstructor() {
+        PersonDTO dto = new PersonDTO();
+        assertNull(dto.getId());
+        assertNull(dto.getName());
+        assertNull(dto.getEmail());
+        assertNull(dto.getPhone());
+        assertNull(dto.getRole());
     }
 }
