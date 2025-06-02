a62a5a2b4f09e402490ee7dd525ce493824bf225
Victor Moreira <you@example.com>
2025-06-02 07:32:01 -0300

    Limpando a branch release

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/tailwind.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/styles/theme.css'
-
-createApp(App).use(router).mount('#app')
