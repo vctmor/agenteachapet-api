42c879243f54dacf09eaa811d3a38e4cfe1ffa19
Victor Moreira <you@example.com>
2025-07-28 12:28:11 -0300

    Add validations and unit tests for PetSearchResponseDTO

--- src/main/java/br/com/liquentec/AgenteAchaPet/dto/response/PetSearchResponseDTO.java
+++ src/main/java/br/com/liquentec/AgenteAchaPet/dto/response/PetSearchResponseDTO.java
@@ -5,27 +5,49 @@ import java.time.LocalDateTime;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import br.com.liquentec.AgenteAchaPet.model.SearchRole;
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PetSearchResponseDTO {
+
+    @NotNull
     private Long id;
+
+    @NotNull
     private Long petId;
+
+    @NotNull
     private Long personId;
+
+    @NotBlank
     private String petName;
+
+    @NotBlank
     private String personName;
+
+    @NotNull
     private SearchRole reporterRole;
+
+    @Email
     private String email;
+
     private String phone;
+
     private String name;
     private String color;
     private String breed;
+
     private Integer age;
+
     private String specialNeed;
+
     private LocalDateTime disappearanceDate;
-    private String location;    
+
+    private String location;
     private byte[] photo;
     private String additionalNotes;
     private String slug;
